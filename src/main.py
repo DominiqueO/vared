57b4f65bc177b822e730e1da6de815697c392079
DominiqueO <DominiqueO@users.noreply.github.com>
2024-07-18 17:02:10 +0200

    Renamed folder with datasets to 'data'

--- src/main.py
+++ src/main.py
@@ -1,6 +1,23 @@
 
 import numpy as np
 import jax.numpy as jnp
+import numpy as np
+import os
+import torch
+from torchvision import datasets
+
+# Import of custom modules
+import datahelper
+import models
+
+
+if __name__ == "__main__":
+    # load data
+    # IJCNN1
+    dirData1 = "../data/ijcnn1/ijcnn1"
+    testData1 = datahelper.load_ijcnn1(dirData1 + ".t")
+    trainData1 = datahelper.load_ijcnn1(dirData1 + ".tr")
+    # CoverType
 
 
 
