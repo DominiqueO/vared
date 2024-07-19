168e51412a6556739b5d17d5ddefee5c0f7baa1e
DominiqueO <DominiqueO@users.noreply.github.com>
2024-07-19 14:01:05 +0200

    Corrected CNN model

--- src/models.py
+++ src/models.py
@@ -1,15 +1,14 @@
-
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-
 class LogReg(nn.Module):
     """
     Logistic regression classifier
     """
+
     def __init__(self, n_inputs, n_outputs):
         """
         Constructor for logistic regression classifier
@@ -28,28 +27,43 @@ class LogReg(nn.Module):
         y_pred = torch.sigmoid(self.linear(x))
         return y_pred
 
+
 class Perceptron():
     """Perceptron for binary classification"""
     ...
 
+
 class SVM():
     """Support vector machine for multiclass classification"""
 
+
 class CNN_simple(nn.Module):
     """Simple convolutional neural network for multiclass classification"""
-    def __init__(self, image_size):
-        super().__init__()
-        self.conv1 = nn.Conv2d(1, 60, 3, padding = 'same', bias = True)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(60, 40, 3, padding = 'same', bias = True)
-        self.fc1 = nn.Linear(40 * (image_size//4)**2, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, image_size**2)
+
+    def __init__(self):
+        super(CNN_simple, self).__init__()
+        self.cnn_layers = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=(3, 3), stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+            nn.Conv2d(32, 64, kernel_size=(3, 3), stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2))
+        )
+
+        self.fc_layers = nn.Sequential(
+            nn.Dropout(p=0.2),
+            nn.Linear(1600, 128),
+            nn.ReLU(),
+            nn.Dropout(p=0.2),
+            nn.Linear(128, 10),
+            nn.Softmax(dim=1)
+        )
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(
+        x = self.cnn_layers(x)
+        # flatten to (batch_size, ... )
+        x = x.view(x.size(0), -1)
+        x = self.fc_layers(x)
+
+        return x
